1371e421e465bbf1d3cfe0e51b27ce23f7ead6eb
fujitaiori <ioriori0413@gmail.com>
2023-02-07 18:50:19 +0900

    記事の更新削除時にインジケータを表示する

--- components/ArticleItem.tsx
+++ components/ArticleItem.tsx
@@ -5,6 +5,7 @@ import {
   Group,
   Menu,
   Button,
+  LoadingOverlay,
 } from "@mantine/core"
 import { Article } from "../types"
 import { useMutateArticle } from "../hooks/useMutateArticles"
@@ -66,76 +67,80 @@ export const ArticleItem: FC<Article> = ({
   const paperColor = '#e6eae3'
 
   return (
-    <Paper shadow="md" p="md" radius={10} withBorder style={{marginTop:"50px", marginBottom:"20px", width: "600px", backgroundColor:paperColor, }}>
-      <div className="flex">
+    <div style={{position: 'relative'}}>
+      <LoadingOverlay visible={updateArticleMutation.isLoading || deleteArticleMutation.isLoading} overlayBlur={2} overlayColor={"1"} radius={10} />
+      <Paper shadow="md" p="md" radius={10} withBorder style={{marginTop:"50px", marginBottom:"20px", width: "600px", backgroundColor:paperColor, }}>
 
-        <Text lineClamp={3} size="lg" weight={900} style={{width:"95%", color:"#333631",}}>{siteTitle}</Text>
-          
-        <Menu shadow="md" width={200}>
-          <Menu.Target>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" style={{cursor:"pointer", color:"#333631"}}>
-              <path fillRule="evenodd" d="M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" clipRule="evenodd" />
-            </svg>
-          </Menu.Target>
+        <div className="flex">
 
-          <Menu.Dropdown>
-            <Menu.Item icon={<IconCopy size={14} />} onClick={copyTextOnClipboard}>Copy</Menu.Item>
-            <Menu.Item icon={<IconExternalLink size={14} />}>
-              <a href={siteUrl} target="_blank" rel="noopener noreferrer" style={{color:"inherit", textDecoration:"none"}}>
-                  Go page
-              </a>
-            </Menu.Item>
-            <Menu.Item color="red" icon={<IconTrash size={14} />} onClick={() => {deleteArticleMutation.mutate(id)}}>Delete</Menu.Item>
+          <Text lineClamp={3} size="lg" weight={900} style={{width:"95%", color:"#333631",}}>{siteTitle}</Text>
+            
+          <Menu shadow="md" width={200}>
+            <Menu.Target>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" style={{cursor:"pointer", color:"#333631"}}>
+                <path fillRule="evenodd" d="M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" clipRule="evenodd" />
+              </svg>
+            </Menu.Target>
 
-          </Menu.Dropdown>
-        </Menu>
-      </div>
+            <Menu.Dropdown>
+              <Menu.Item icon={<IconCopy size={14} />} onClick={copyTextOnClipboard}>Copy</Menu.Item>
+              <Menu.Item icon={<IconExternalLink size={14} />}>
+                <a href={siteUrl} target="_blank" rel="noopener noreferrer" style={{color:"inherit", textDecoration:"none"}}>
+                    Go page
+                </a>
+              </Menu.Item>
+              <Menu.Item color="red" icon={<IconTrash size={14} />} onClick={() => {deleteArticleMutation.mutate(id)}}>Delete</Menu.Item>
 
-      <hr style={{borderTop: "0.5px solid rgba(0, 0, 0, 0.1)", borderBottom: "0.5px solid rgba(255, 255, 255, 0.2)"}}></hr>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
 
-      <textarea 
-        id={id + '-textarea'}
-        defaultValue={text}
-        readOnly
-        rows={3} 
-        style={{
-          overflow:"hidden", 
-          width:"100%", 
-          resize:"none",
-          backgroundColor:paperColor,
-          color:"#333631",
-          border:"none",
-          display: "-webkit-box",
-          WebkitBoxOrient: "vertical",
-          WebkitLineClamp:3,
-          cursor: "pointer", 
-          outline: "none",
-          lineHeight: 1.7,
-          fontFamily: 'Inter',
-          letterSpacing: 1.5,
-        }}
-        onClick={editableTextarea}
-        onChange={(e) => {
-          setText(e.target.value)
-        }}
-      />
-      
-      <div className="" style={{height:"30px", marginTop:"15px"}}>
+        <hr style={{borderTop: "0.5px solid rgba(0, 0, 0, 0.1)", borderBottom: "0.5px solid rgba(255, 255, 255, 0.2)"}}></hr>
 
-        <Group position="right" spacing="lg">
+        <textarea 
+          id={id + '-textarea'}
+          defaultValue={text}
+          readOnly
+          rows={3} 
+          style={{
+            overflow:"hidden",
+            width:"100%", 
+            resize:"none",
+            backgroundColor:paperColor,
+            color:"#333631",
+            border:"none",
+            display: "-webkit-box",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp:3,
+            cursor: "pointer", 
+            outline: "none",
+            lineHeight: 1.7,
+            fontFamily: 'Inter',
+            letterSpacing: 1.5,
+          }}
+          onClick={editableTextarea}
+          onChange={(e) => {
+            setText(e.target.value)
+          }}
+        />
+        
+        <div className="" style={{height:"30px", marginTop:"15px"}}>
 
-          <Button id={id + '-cancel'} className="cancel-button" size="xs" onClick={reset} style={{backgroundColor:"#90A4AE", visibility:"hidden" }}>
-            Reset
-          </Button>
+          <Group position="right" spacing="lg">
 
-          <Button id={id + '-save'} className="save-button" size="xs" onClick={updateArticle} style={{backgroundColor:"#448AFF", visibility:"hidden"}}>
-            Save
-          </Button>
+            <Button id={id + '-cancel'} className="cancel-button" size="xs" onClick={reset} style={{backgroundColor:"#90A4AE", visibility:"hidden" }}>
+              Reset
+            </Button>
 
-        </Group>
+            <Button id={id + '-save'} className="save-button" size="xs" onClick={updateArticle} style={{backgroundColor:"#448AFF", visibility:"hidden"}}>
+              Save
+            </Button>
 
-      </div>
-    </Paper>
+          </Group>
+
+        </div>
+      </Paper>
+    </div>
 
   )
 }
